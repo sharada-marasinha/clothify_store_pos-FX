18ab29b5f993d09cb53858b8a9b56ba8c7a6063c
sharada-marasinha <sharadamarasinha@gmail.com>
2023-10-10 22:29:37 +0530

    dash_bord_form.fxml

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,8 +3,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
@@ -13,6 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("login_form.fxml"))));
+        stage.setResizable(false);
         stage.show();
     }
 }
--- src/main/java/edu/icet/controller/LoginFormController.java
+++ src/main/java/edu/icet/controller/LoginFormController.java
@@ -1,4 +1,14 @@
 package edu.icet.controller;
 
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+
 public class LoginFormController {
+    public JFXTextField txtEmail;
+    public JFXPasswordField txtPassword;
+
+    public void btnLoginOnAction(ActionEvent actionEvent) {
+
+    }
 }
